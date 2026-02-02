4d12a2cef2bfe86cd117b655dac28f7514920e3b
Igor Santos <igorsantos69142@gmail.com>
2026-02-02 19:44:39 -0400

    Add SQL queries for problems 595 and 1148

--- sql/easy/#2_584. Find Customer Referee.sql
+++ sql/easy/#2_584. Find Customer Referee.sql
@@ -1,4 +1,5 @@
 -- Link to the original problem: https://leetcode.com/problems/find-customer-referee/
+-- Link usado para resolver parte do problema: https://www-w3schools-com.translate.goog/mysql/mysql_operators.asp?_x_tr_sl=en&_x_tr_tl=pt&_x_tr_hl=pt&_x_tr_pto=tc
 -- Write your MySQL query statement below
 SELECT
 name
---
+++ sql/easy/#4_1148. Article Views I.sql
@@ -0,0 +1,7 @@
+-- Link to the original problem: https://leetcode.com/problems/article-views-i/
+-- Write your MySQL query statement below
+SELECT DISTINCT
+author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id ASC
